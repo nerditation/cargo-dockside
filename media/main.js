13124de61655429ccc6982a5ec1d86f19d4e2372
nerditation <nerditation@users.noreply.github.com>
2025-03-23 22:56:08 +0800

    prototyping recent commands

--- media/main.js
+++ media/main.js
@@ -2,6 +2,33 @@
   const vscode = acquireVsCodeApi();
   const commandCategoriesDiv = document.getElementById("commandCategories");
 
+  const recent_commands_section = document.createElement("div");
+  recent_commands_section.className = "category-section";
+  recent_commands_section.id = `RecentCommandsSection`; // ID for "recent" section
+
+  const recent_commands_header = document.createElement("h3");
+  recent_commands_header.textContent = "Recent Commands";
+  recent_commands_section.appendChild(recent_commands_header);
+
+  commandCategoriesDiv.appendChild(recent_commands_section);
+
+  function addToRecent(clicked) {
+    // if already in recent list, remove it first
+    for (let child = recent_commands_section.firstElementChild; child; child = child.nextElementSibling) {
+      if (child.getAttribute("data-command") === clicked.getAttribute("data-command")) {
+        recent_commands_section.removeChild(child);
+        break;
+      }
+    }
+    // if already have 5 commands in the recent list, pop the last one
+    // childElementCount include the h3 header
+    if (recent_commands_section.childElementCount === 1 + 5) {
+      recent_commands_section.removeChild(recent_commands_section.lastElementChild);
+    }
+    // insert the button after the h3 header element, but there's no insertAfter()
+    recent_commands_section.insertBefore(clicked, recent_commands_header.nextElementSibling);
+  }
+
   for (const [category, commands] of Object.entries(categories)) {
     const section = document.createElement("div");
     section.className = "category-section";
@@ -21,10 +48,13 @@
       // Set button color based on category
       button.classList.add(`${category.replace(/\s+/g, "")}-button`);
 
-      button.addEventListener("click", () => {
+      button.onclick = () => {
         console.log(`Button clicked: ${command.id}`); // Debug log
+        const cloned = button.cloneNode(true);
+        cloned.onclick = button.onclick;
+        addToRecent(cloned);
         vscode.postMessage({ command: command.id });
-      });
+      };
 
       section.appendChild(button);
     });
